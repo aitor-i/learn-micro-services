1a6de430e519f39dcd8271904689737561479697
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-07-05 21:10:38 +0200

    DELL(CommentList) - Title

--- 1-project/client/src/components/PostList/CommentList/CommentList.jsx
+++ 1-project/client/src/components/PostList/CommentList/CommentList.jsx
@@ -2,7 +2,6 @@ export const CommentList = ({ comments }) => {
   console.log("comments", comments);
   return (
     <div>
-      <h5>Comments:</h5>
       {comments?.map((comment) => (
         <div key={comment.id}>
           <p>{comment.content}</p>
