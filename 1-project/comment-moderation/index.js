0e8c03630ab563f6366f5cb9d177de19e08aba25
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-07-07 13:35:11 +0200

    ADD(Comment-moderation) - Event listener

--- 1-project/comment-moderation/index.js
+++ 1-project/comment-moderation/index.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(cors());
 
+const commentsForModeration = {};
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+  if (type === "CommentCreated") res.status(404);
+});
+
 app.listen(4004, () => {
   console.log("Listening on port 4004!");
 });
