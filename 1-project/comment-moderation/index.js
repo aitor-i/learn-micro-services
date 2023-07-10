42ba5c9286d242eef6f90113e5e5847b329c31b9
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-07-10 23:54:21 +0200

    ADD(Comment-moderation) - CommentCreated method

--- 1-project/comment-moderation/index.js
+++ 1-project/comment-moderation/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 
+import axios from "axios";
+
 import cors from "cors";
 
 const app = express();
@@ -8,9 +10,19 @@ app.use(cors());
 
 const commentsForModeration = {};
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   const { type, data } = req.body;
-  if (type === "CommentCreated") res.status(404);
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    const newData = { ...data, status };
+    await axios.post("http://localhost/4005/events", {
+      type: "CommentModerated",
+      data: newData,
+    });
+    console.log(data);
+  }
+  res.status(404);
 });
 
 app.listen(4004, () => {
